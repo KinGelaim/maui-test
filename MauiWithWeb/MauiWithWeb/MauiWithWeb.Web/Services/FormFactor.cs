88372049b5e3dabe7bf44007175b2f444622b149
KinGelaim <89123232156@mail.ru>
2025-09-14 15:35:59 +0500

    Added a template for MAUI with a Web connection and configured for my Android

---
+++ MauiWithWeb/MauiWithWeb/MauiWithWeb.Web/Services/FormFactor.cs
@@ -0,0 +1,17 @@
+using MauiWithWeb.Shared.Services;
+
+namespace MauiWithWeb.Web.Services
+{
+    public class FormFactor : IFormFactor
+    {
+        public string GetFormFactor()
+        {
+            return "Web";
+        }
+
+        public string GetPlatform()
+        {
+            return Environment.OSVersion.ToString();
+        }
+    }
+}
---
+++ MauiWithWeb/MauiWithWeb/MauiWithWeb/Services/FormFactor.cs
@@ -0,0 +1,17 @@
+using MauiWithWeb.Shared.Services;
+
+namespace MauiWithWeb.Services
+{
+    public class FormFactor : IFormFactor
+    {
+        public string GetFormFactor()
+        {
+            return DeviceInfo.Idiom.ToString();
+        }
+
+        public string GetPlatform()
+        {
+            return DeviceInfo.Platform.ToString() + " - " + DeviceInfo.VersionString;
+        }
+    }
+}
